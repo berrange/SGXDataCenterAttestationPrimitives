83e63c9e9c84f3543a6252ed725aad19dd3b489f
Daniel P. Berrangé <berrange@redhat.com>
2024-08-29 12:22:56 +0100

    Enable pointing sgxssl build to alternative glibc headers
    The enclave must be built with headers from a specific glibc version
    for the sake of reproducibility. It does not actually link to glibc,
    merely requiring a few self contained definitions.
    
    The ENCLAVE_SYSTEM_INCLUDES make var can be set from RPM builds.
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- QuoteVerification/prepare_sgxssl.sh
+++ QuoteVerification/prepare_sgxssl.sh
@@ -94,7 +94,7 @@ pushd $sgxssl_dir/Linux/
 if [[ "$*" == *_TD_MIGRATION* ]];then
 make clean sgxssl_no_mitigation NO_THREADS=1 LINUX_SGX_BUILD=2 _TD_MIGRATION=1
 else
-make clean sgxssl_no_mitigation 
+make clean sgxssl_no_mitigation ENCLAVE_SYSTEM_INCLUDES="$ENCLAVE_SYSTEM_INCLUDES"
 fi
 popd
 
