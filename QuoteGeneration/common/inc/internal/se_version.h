1db2f71aead55201fcd82efa7d1ee99c9fa006b9
Daniel P. Berrangé <berrange@redhat.com>
2025-01-17 15:39:39 +0000

    Fix soname version for libsgx_qe3_logic.so library
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- QuoteGeneration/common/inc/internal/se_version.h
+++ QuoteGeneration/common/inc/internal/se_version.h
@@ -41,6 +41,11 @@
 #define QUOTE_LOADER_VERSION         "1.11.109.1"
 #define TDQE_WRAPPER_VERSION         "1.14.109.1"
 #define PCE_WRAPPER_VERSION          "1.14.109.1"
+/*
+ * XXX: downstream hack based on version declared
+ * in linux-sgx.git/linux/installer/common/psw/Makefile
+ */
+#define QE3_WRAPPER_VERSION          "1.0.0"
 
 #define QE3_VERSION                  "1.19.100.1"
 #define QVE_VERSION                  "1.21.100.1"
