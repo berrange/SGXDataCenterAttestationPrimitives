307b0ff6d27b6788cdc4b4f4abac9e949129c19e
Daniel P. Berrangé <berrange@redhat.com>
2025-12-04 18:05:14 +0000

    pcsclient: ignore errors trying to clear the keyring
    On authentication errors with PCS, an attempt is made to clear the
    keyring. This may fail if the user's login environment has no keyring
    configured. The user would have declined to store the key when first
    prompted, so there would be nothing to clear either in this case.
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- tools/PcsClientTool/lib/intelsgx/pcs.py
+++ tools/PcsClientTool/lib/intelsgx/pcs.py
@@ -404,7 +404,13 @@ class PCS:
         if response.status_code != 200:
             print(str(response.content, 'utf-8'))
             if response.status_code == 401:
-                Credentials().set_pcs_api_key('')   #reset ApiKey
+                try:
+                    Credentials().set_pcs_api_key('')   #reset ApiKey
+                except:
+                    # If keyring is unavailable, we don't want to trigger
+                    # traceback, as the user may have declined to save
+                    # the key in the keyring earlier
+                    pass
             return None
 
         # Verify expected headers
@@ -479,7 +485,13 @@ class PCS:
         if response.status_code != 200:
             print(str(response.content, 'utf-8'))
             if response.status_code == 401:
-                Credentials().set_pcs_api_key('')   #reset ApiKey
+                try:
+                    Credentials().set_pcs_api_key('')   #reset ApiKey
+                except:
+                    # If keyring is unavailable, we don't want to trigger
+                    # traceback, as the user may have declined to save
+                    # the key in the keyring earlier
+                    pass
             return None
 
         # Verify expected headers
