48f3dc21602f2f11f054c740c5efd4c34d5efae6
Daniel P. Berrangé <berrange@redhat.com>
2025-12-04 18:05:14 +0000

    pccsadmin: ignore errors trying to clear the keyring
    On authentication errors with PCS, an attempt is made to clear the
    keyring. This may fail if the user's login environment has no keyring
    configured. The user would have declined to store the key when first
    prompted, so there would be nothing to clear either in this case.
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- tools/PccsAdminTool/pccsadmin.py
+++ tools/PccsAdminTool/pccsadmin.py
@@ -166,7 +166,13 @@ class PccsClient:
             if response.status_code == 200:
                 self._write_output_file(output_file, response)
             elif response.status_code == 401:  # Authentication error
-                self.credentials.set_admin_token('')
+                try:
+                    self.credentials.set_admin_token('')
+                except:
+                    # If keyring is unavailable, we don't want to trigger
+                    # traceback, as the user may have declined to save
+                    # the key in the keyring earlier
+                    pass
                 print("Authentication failed.")
             else:
                 self._handle_error(response)
@@ -196,7 +202,13 @@ class PccsClient:
                 if response.status_code == 200:
                     print("Collaterals uploaded successfully.")
                 elif response.status_code == 401:  # Authentication error
-                    self.credentials.set_admin_token('')
+                    try:
+                        self.credentials.set_admin_token('')
+                    except:
+                        # If keyring is unavailable, we don't want to trigger
+                        # traceback, as the user may have declined to save
+                        # the key in the keyring earlier
+                        pass
                     print("Authentication failed.")
                 else:
                     self._handle_error(response)
@@ -212,7 +224,13 @@ class PccsClient:
                 if response.status_code == 200:
                     print("Policy uploaded successfully with policy ID :" + response.text)
                 elif response.status_code == 401:  # Authentication error
-                    self.credentials.set_admin_token('')
+                    try:
+                        self.credentials.set_admin_token('')
+                    except:
+                        # If keyring is unavailable, we don't want to trigger
+                        # traceback, as the user may have declined to save
+                        # the key in the keyring earlier
+                        pass
                     print("Authentication failed.")
                 else:
                     self._handle_error(response)
@@ -245,7 +263,13 @@ class PccsClient:
             if response.status_code == 200:
                 print("The cache database was refreshed successfully.")
             elif response.status_code == 401:  # Authentication error
-                self.credentials.set_admin_token('')
+                try:
+                    self.credentials.set_admin_token('')
+                except:
+                    # If keyring is unavailable, we don't want to trigger
+                    # traceback, as the user may have declined to save
+                    # the key in the keyring earlier
+                    pass
                 print("Authentication failed.")
             else:
                 self._handle_error(response)
